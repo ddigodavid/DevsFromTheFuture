f5e0b1eaff73027d1a289683ad7bd98592ce164e
Wendel Orti Guirao <wendel.o.g@gmail.com>
2017-07-26 22:58:46 -0300

    terceiro post

--- Laravel/resources/views/posts/index.blade.php
+++ Laravel/resources/views/posts/index.blade.php
@@ -13,14 +13,6 @@
         {!! $post->description !!}
         
         <img src="{{ asset($post->imagePath) }}" alt="" width="100%" style="margin-top: 30px">
-        
-        {{--<p>E você, está preparado para ser um desenvolvedor do futuro?</p>--}}
-        {{----}}
-        {{--<p>Torne-se um de nós. receba na sua caixa de entrada referências sobre três conhecimentos essenciais para qualquer--}}
-            {{--desenvolvedor do futuro.--}}
-            {{--são conceitos, artigos, cursos e livros selecionados, sobre assuntos que você não pode deixar de saber.--}}
-            {{--a partir de agora, é com você.--}}
-            {{--see you soon.</p>--}}
     </div>
     <div class="col-sm-12 col-md-3 col-md-offset-1 sidebar-right text-justify">
         <div class="container"></div>
--- Laravel/resources/views/posts/test.blade.php
+++ Laravel/resources/views/posts/test.blade.php
@@ -7,34 +7,79 @@
 @endsection
 
 @section('content')
-    <div id="post" class="col-xs-12 col-xs-offset-0 col-md-7 col-md-offset-1 text-justify">
-        <h1>Construindo diversidade: como empoderar mulheres para programar</h1>
+    <div id="post" class="col-xs-12 col-xs-offset-0 col-md-7 col-md-offset-1">
+        <h1>
+            Construindo diversidade:
+            como empoderar mulheres para programar
+        </h1>
     
-        <p>De acordo com o censo de 2010, as mulheres representam apenas 25% dos programadores. Apesar de um histórico de pouca participação na área, nos últimos anos as coisas têm mudado. As mulheres têm passado de observadoras à protagonistas, porém os números ainda assustam. Conforme as fontes:</p>
+        <img src="{{ asset('images/posts/construindo-diversidade-como-empoderar-mulheres-para-programar.jpg') }}" alt="" width="100%" style="margin-top: 30px">
+    
+        <p style="padding-top:20px;">
+            De acordo com o censo de 2010, as mulheres representam apenas 25% dos programadores. Apesar de um histórico de pouca participação na área, nos últimos anos as coisas têm mudado. As mulheres têm passado de observadoras à protagonistas, porém os números ainda assustam. Conforme as fontes:
+        </p>
     
         <div class="row">
-            <div class="col-xs-12 col-sm-3">
+            <div class="col-sm-12 col-md-4">
                 Apenas
-                15,53%
-                dos alunos de cursos relacionados à computação são mulheres.
-                (Fonte: Inep/MEC)
+                <p>
+                        15,53%
+                    <br>dos alunos de cursos relacionados à computação são mulheres.
+                    <br>(Fonte: Inep/MEC)
+                </p>
+                </span>
             </div>
-            <div class="col-xs-12 col-sm-offset-1 col-sm-3">
-                41%
-                das mulheres que trabalham com tecnologia acabam deixando a área, em comparação com apenas 17% dos homens.
-                (Fonte: Harvard Business Review)
+            <div class="col-sm-12 col-md-4">
+                <p>41%
+                    <br>das mulheres que trabalham com tecnologia acabam deixando a área, em comparação com apenas 17% dos homens.
+                    <br>(Fonte: Harvard Business Review)
+                </p>
             </div>
-            <div class="col-xs-12 col-sm-offset-1 col-sm-3">
-            79%
-            das alunas dos cursos relacionados à Tecnologia da Informação desistem no primeiro ano
-            (Fonte: Gizmodo)
+            <div class="col-sm-12 col-md-4">
+                <p>79%
+                    <br>das alunas dos cursos relacionados à Tecnologia da Informação desistem no primeiro ano
+                    <br>(Fonte: Gizmodo)
+                </p>
             </div>
         </div>
-
+        
+        <p>
+            Estereótipos reforçam a ideia de que a tecnologia é uma área masculina, e essa falta de identificação fortalecem ainda mais os índices baixos.  Empoderar é necessário. Para contornar o problema, várias iniciativas têm surgido nos últimos anos.  Entre elas algumas se destacam:
+        </p>
+    
+        <a class="pink-label" href="https://www.programaria.org/">Programaria</a>
+            <p>
+                Depois de muita discussão e reflexão sobre nossa trajetória, identificamos padrões diferentes de problemas que as mulheres costumam enfrentar na relação com a programação. De toda essa inquietação nasceu a PrograMaria, um meta-site sobre mulheres e tecnologia. A PrograMaria é um convite para refletir, se inspirar e aprender.
+            </p>
+        
+        <a class="pink-label"href="http://brasil.pyladies.com/about/">Pyladies</a>
+        <p>
+            O PyLadies é uma comunidade mundial que foi trazida ao Brasil com o propósito de instigar mais mulheres a entrarem na área tecnológica. Queremos mudar essa realidade de poucas garotas em uma área tão rica e fantásticas como a computação.
+        </p>
+    
+        <a class="pink-label"href="https://mulheresnacomputacao.com/">Mulheres na Computação</a>
+        <p>
+            Criada por Camila Achutti, tem por objetivo educar as meninas sobre o mundo da computação. O blog oferece dicas de programação, quadro de oportunidades e muito mais.
+        </p>
+    
+        <a class="pink-label"href="https://www.womentechmakers.com/">Women Techmakers</a><span> do Google</span>
+        <p>
+            Visa aumentar a participação das mulheres na área da tecnologia. Procura incentivar as mulheres a se tornarem líderes e exemplos no setor.
+        </p>
+    
+        <a class="pink-label"href="http://www.eupossoprogramar.com/">Pyladies</a><span> para meninas da Microsoft</span>
+        <p>
+            Iniciativa da Microsoft para incentivar a programação por parte das meninas. Começou como um projeto geral, mas esse ano foi lançado o projeto só para meninas.
+            <br>
+            O melhor de tudo é que não existe uma idade certa para aprender. Afinal, nunca é cedo demais para sair da superfície e se diferenciar no meio da multidão.
+        </p>
     </div>
+    
+    <br><br>
+    
     <div class="col-sm-12 col-md-3 col-md-offset-1 sidebar-right text-justify">
         <div class="container"></div>
-        
+    
         <div class="container call-to-action-posts">
             <div class="full-height fill-width">
                 <a href="{{ sprintf("%s%s", route('register.form'), '?utm_source=website&utm_campaign=Bot%C3%A3o%20Commit') }}" class="fill-height">
@@ -46,14 +91,14 @@
                         </div>
                     </div>
                     <div class="col-sm-12 half-height v-align-content-center">
-                        <span class="dark-blue-label commit-here-button">
-                            <div class="row">
-                                <div class="col-sm-7">
-                                    [&nbsp;&nbsp;&nbsp;COMMIT&nbsp;&nbsp;&nbsp;]
-                                </div>
+                    <span class="dark-blue-label commit-here-button">
+                        <div class="row">
+                            <div class="col-sm-7">
+                                [&nbsp;&nbsp;&nbsp;COMMIT&nbsp;&nbsp;&nbsp;]
                             </div>
-                            {{--<a href="{{ route('register.form') }}">[&nbsp;&nbsp;&nbsp;COMMIT HERE&nbsp;&nbsp;&nbsp;]</a>--}}
-                        </span>
+                        </div>
+                        {{--<a href="{{ route('register.form') }}">[&nbsp;&nbsp;&nbsp;COMMIT HERE&nbsp;&nbsp;&nbsp;]</a>--}}
+                    </span>
                     </div>
                 </a>
             </div>
